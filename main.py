54a5b861f4835db1ef903dec739246a63b515f72
lukas <mail@lukas-eisenhauer.de>
2023-03-19 18:40:09 +0100

    simplify terms if possible

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ from variable import Symbol
 x = Symbol('x')
 y = Symbol('y')
 
-print(3*x+2*y+x * y)
+print(3*x+2*y+x * y*2*3)
--- variable.py
+++ variable.py
@@ -5,6 +5,9 @@ class Calculatable:
     def __radd__(self, other):
         return Addition([self, other])
 
+    def __sub__(self, other):
+        return Addition([self, Product([-1, other])])
+
     def __mul__(self, other):
         return Product([self, other])
 
@@ -25,24 +28,43 @@ class Term(Calculatable):
 class CommutableTerm(Term):
     def __init__(self, parts = []):
         self.parts = parts
+        self.simplify()
 
     def simplify(self):
-        for (index, part) in enumerate(self.parts):
-            if type(part) == Addition:
-                del self.parts[index]
-                self.parts.extend(part.parts)
+        # simplify all parts
+        for part in self.parts:
+            if isinstance(part, Term):
+                part.simplify()
+        # flatten redundant layers
+        additionalParts = sum([part.parts for part in self.parts if type(part) == type(self)], [])
+        self.parts[:] = [part for part in self.parts if type(part) != type(self)] + additionalParts
+        # simplify numerical values
+        numericalValue = self.defaultValue
+        for part in self.parts:
+            if isinstance(part, (int, float)):
+                numericalValue = self.action(numericalValue, part)
+        self.parts[:] = [part for part in self.parts if not isinstance(part, (int, float))]
+        if numericalValue != self.defaultValue:
+             self.parts += [numericalValue]
+        return self
 
     def __str__(self):
-        return f'{self.symbol.join([str(part) for part in self.parts])}'
+        return f'({self.symbol.join([str(part) for part in self.parts])})'
 
 
 class Addition(CommutableTerm):
     symbol = '+'
+    defaultValue = 0
+
+    def action(self, x,y): return x+y
 
 class Product(CommutableTerm):
     symbol = '*'
+    defaultValue = 1
 
-class Symbol(Calculatable):
+    def action(self, x,y): return x*y
+
+class Symbol(Term):
     def __init__(self, name, dimensions=1):
         self.name = name
         self.dimensions = dimensions
